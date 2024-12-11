a93592fe75b7efa40a393058f17fb27d25cdf638
lanre adefolaju <lanre3c@gmail.com>
2024-12-10 22:10:08 -0500

    latest

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,26 @@ import { Navbar, Nav } from "react-bootstrap";
 const NavBar = () => {
   return (
   <div className="container justify-content-center">
-    <Navbar bg="white" variant="white" expand="lg">
-      <Navbar.Brand as={Link} to="/">Peak Rentals</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-          <Nav.Link as={Link} to="/equipment">Equipment</Nav.Link>
-          <Nav.Link as={Link} to="/contact-us">Contact Us</Nav.Link>
-          <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
   </div>
   );
 };
